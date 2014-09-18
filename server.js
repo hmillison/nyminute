c70bb0a638298a936d3deedd080a0527f0502487
Henry Millison <hmillison@gmail.com>
2014-09-18 01:51:03 -0400

    get stoptimes by id implemented, cleaned up directory

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ var fs = require('fs'),
 express = require('express'),
 app = express();
 var request = require('request')
-
-var example = __dirname + '/example.json';
+var Parse = require('parse').Parse;
+var moment = require('moment');
 var Converter=require("csvtojson").core.Converter;
 var fs=require("fs");
 
 // load auth variables
 var auth = require('./auth');
 
+Parse.initialize(auth.parse.id, auth.parse.key);
+
 var port = process.env.PORT || 3000;    // set our port
 
 var router = express.Router();
 
 app.use(express.static(__dirname + '/public'));
+var example = __dirname + '/example.json';
 
 
 app.all('*', function(req, res, next) {
@@ -23,16 +26,14 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-
-
 app.get('/',function(req,res){
   res.render('index');
   
 });
 
 //Converts MTA Schedule Data from a CSV to JSON
-app.get('/getcsvjson/:file', function(req,res){
-	var csvFileName="./data/" + req.param("file") + ".csv";
+app.get('/getcsvjson', function(req,res){
+	var csvFileName="./data/" + req.query.file + ".csv";
 	var fileStream=fs.createReadStream(csvFileName);
 	var csvConverter=new Converter({constructResult:true});
 	//end_parsed will be emitted once parsing finished
@@ -42,6 +43,28 @@ app.get('/getcsvjson/:file', function(req,res){
 
 	//read from file
 	fileStream.pipe(csvConverter);	
+});
+
+var hello;
+app.get('/getstoptimes', function(req,res){
+	var min = req.query.minutes
+	if(min == undefined){
+		min = 15;
+	}
+	var currenttime = moment().format('HH:mm:ss');
+	var comparetime = moment().add(min,'m').format('HH:mm:ss');
+	var StopTime = Parse.Object.extend("stop_times");
+	var query = new Parse.Query(StopTime);
+	query.equalTo("stop_id", req.query.stopid);
+	query.greaterThan("arrival_time",currenttime);
+	query.lessThan("arrival_time",comparetime);
+	query.find({
+	  success: function(data) {
+	  	res.send(data)
+	  },
+	  error: function(error) {
+	  }
+	});
 });
 
 app.use('/', router);
